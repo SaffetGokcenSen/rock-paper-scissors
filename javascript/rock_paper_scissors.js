ab1b5b348a9415941f2a5e0f7e043bc956fcce6c
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-03-15 20:58:06 +0300

    simplified the code by defining a function

--- javascript/rock_paper_scissors.js
+++ javascript/rock_paper_scissors.js
@@ -39,37 +39,34 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-    for (i = 0; i < 5; ++i) {
-        let playerSelection = prompt("please enter one of rock, paper or scissors", "rock")
-        playerSelection = playerSelection.toLowerCase();
-
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        if (result === "tied") {
-            computerScore += 1;
-            playerScore += 1;
-        }
-        else if (result === "you") {
-            playerScore += 1;
-        }
-        else {
-            computerScore += 1;
-        }
-        console.log("The Computer is " + computerSelection + " and the player is " + playerSelection + ".");
+function playRoundUserInterface(result) {
+    if (result === "tied") {
+        computerScore += 0;
+        playerScore += 0;
     }
-    if (computerScore > playerScore) {
-        console.log("The computer wins " + computerScore + " to " + playerScore + ".");
-    }
-    else if ( computerScore < playerScore) {
-        console.log("The player wins " + playerScore + " to " + computerScore + ".");
+    else if (result === "you") {
+        playerScore += 1;
     }
     else {
-        console.log("There is a tie: " + computerScore + " to " + playerScore + ".");
+        computerScore += 1;
     }
-}
+    
+    yourShake.textContent = playerSelection.toUpperCase();
+    computerShake.textContent = computerSelection.toUpperCase();
+    yourScoreBoard.textContent = playerScore;
+    computerScoreBoard.textContent = computerScore;
+
+    if (computerScore == 5) {
+        winnerAnnouncer.textContent = "THE WINNER IS THE COMPUTER BY " + computerScore + " to " + playerScore;
+        computerScore = 0;
+        playerScore = 0;
+    }
+    else if (playerScore == 5) {
+        winnerAnnouncer.textContent = "THE WINNER IS YOU BY " + playerScore + " to " + computerScore;
+        computerScore = 0;
+        playerScore = 0;
+    }
+};
 
 const yourShake = document.querySelector(".player-result").lastChild;
 const computerShake = document.querySelector(".computer-result").lastChild;
@@ -88,34 +85,8 @@ buttonRock.addEventListener("click", () => {
     playerSelection = "rock";
 
     let result = playRound("rock", computerSelection);
-    
-    if (result === "tied") {
-        computerScore += 0;
-        playerScore += 0;
-    }
-    else if (result === "you") {
-        playerScore += 1;
-    }
-    else {
-        computerScore += 1;
-    }
-    console.log("The Computer is " + computerSelection + " and the player is rock.");
-    console.log("The computer is " + computerScore + " and the player is " + playerScore + ".");
-    yourShake.textContent = playerSelection.toUpperCase();
-    computerShake.textContent = computerSelection.toUpperCase();
-    yourScoreBoard.textContent = playerScore;
-    computerScoreBoard.textContent = computerScore;
 
-    if (computerScore == 5) {
-        winnerAnnouncer.textContent = "THE WINNER IS THE COMPUTER BY " + computerScore + " to " + playerScore;
-        computerScore = 0;
-        playerScore = 0;
-    }
-    else if (playerScore == 5) {
-        winnerAnnouncer.textContent = "THE WINNER IS YOU BY " + playerScore + " to " + computerScore;
-        computerScore = 0;
-        playerScore = 0;
-    }
+    playRoundUserInterface(result);
 });
 
 const buttonPaper = document.querySelector("button.paper");
@@ -125,34 +96,8 @@ buttonPaper.addEventListener("click", () => {
     playerSelection = "paper";
     
     let result = playRound("paper", computerSelection);
-    
-    if (result === "tied") {
-        computerScore += 0;
-        playerScore += 0;
-    }
-    else if (result === "you") {
-        playerScore += 1;
-    }
-    else {
-        computerScore += 1;
-    }
-    console.log("The Computer is " + computerSelection + " and the player is paper.");
-    console.log("The computer is " + computerScore + " and the player is " + playerScore + ".");
-    yourShake.textContent = playerSelection.toUpperCase();
-    computerShake.textContent = computerSelection.toUpperCase();
-    yourScoreBoard.textContent = playerScore;
-    computerScoreBoard.textContent = computerScore;
 
-    if (computerScore == 5) {
-        winnerAnnouncer.textContent = "THE WINNER IS THE COMPUTER BY " + computerScore + " to " + playerScore;
-        computerScore = 0;
-        playerScore = 0;
-    }
-    else if (playerScore == 5) {
-        winnerAnnouncer.textContent = "THE WINNER IS YOU BY " + playerScore + " to " + computerScore;
-        computerScore = 0;
-        playerScore = 0;
-    }
+    playRoundUserInterface(result);
 });
 
 const buttonScissors = document.querySelector("button.scissors");
@@ -162,32 +107,6 @@ buttonScissors.addEventListener("click", () => {
     playerSelection = "scissors";
     
     let result = playRound("scissors", computerSelection);
-    
-    if (result === "tied") {
-        computerScore += 0;
-        playerScore += 0;
-    }
-    else if (result === "you") {
-        playerScore += 1;
-    }
-    else {
-        computerScore += 1;
-    }
-    console.log("The Computer is " + computerSelection + " and the player is scissors.");
-    console.log("The computer is " + computerScore + " and the player is " + playerScore + ".");
-    yourShake.textContent = playerSelection.toUpperCase();
-    computerShake.textContent = computerSelection.toUpperCase();
-    yourScoreBoard.textContent = playerScore;
-    computerScoreBoard.textContent = computerScore;
 
-    if (computerScore == 5) {
-        winnerAnnouncer.textContent = "THE WINNER IS THE COMPUTER BY " + computerScore + " to " + playerScore;
-        computerScore = 0;
-        playerScore = 0;
-    }
-    else if (playerScore == 5) {
-        winnerAnnouncer.textContent = "THE WINNER IS YOU BY " + playerScore + " to " + computerScore;
-        computerScore = 0;
-        playerScore = 0;
-    }
+    playRoundUserInterface(result);
 });
